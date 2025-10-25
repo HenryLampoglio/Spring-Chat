57ac0c7b7bcc3aa5ce0284c609f3242732121b95
Henry Lampoglio <henrylampoglio@hotmail.com>
2025-10-25 14:27:55 -0300

    feat: alterando logica das migrations pois notei que nao havia necessidade em duas tabelas de conexao

--- src/main/resources/db/migration/V1_0_1__create_user_connections_table.sql
+++ src/main/resources/db/migration/V1_0_1__create_user_connections_table.sql
@@ -1,7 +1,15 @@
+CREATE type conn_type as enum
+(
+    'accepted',
+    'pending',
+    'blocked'
+);
+
 CREATE TABLE user_connections (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
     user_id UUID REFERENCES users(id) NOT NULL,
     friend_id UUID REFERENCES users(id) NOT NULL,
+    connection_status conn_type NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     UNIQUE (user_id, friend_id)
--- src/main/resources/db/migration/V1_0_2__create_user_pending_connections_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user_pending_connections (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-    initiator_user_id UUID REFERENCES users(id) NOT NULL,
-    requested_user_id UUID REFERENCES users(id) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    UNIQUE (initiator_user_id, requested_user_id)
-);
