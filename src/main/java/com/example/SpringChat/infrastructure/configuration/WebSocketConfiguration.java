ee2d4c5d59075aac0a773ec97aa448270f2491f4
Henry Lampoglio <henrylampoglio@hotmail.com>
2025-12-19 21:37:53 -0300

    corrigindo e configurando devidamente o socket

--- src/main/java/com/example/SpringChat/infrastructure/configuration/WebSocketConfiguration.java
+++ src/main/java/com/example/SpringChat/infrastructure/configuration/WebSocketConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
@@ -23,5 +24,12 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
         registry.enableSimpleBroker("/queue", "/topic")
                 .setHeartbeatValue(new long[] {10000, 20000})
                 .setTaskScheduler(this.messageBrokeTaskScheduler);
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 }
