86147384b4a3f4e5c8d49a667229d606a8c94b3f
HenryLampoglio <henrylampoglio@hotmail.com>
2026-01-13 18:15:53 -0300

    feat: configurando primeira porta websocket

--- src/main/java/com/example/SpringChat/infrastructure/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/SpringChat/infrastructure/configuration/SecurityConfiguration.java
@@ -32,6 +32,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.GET,"swagger-ui/**","/swagger-ui.html", "/v3/api-docs/**").permitAll()
+                        .requestMatchers(req -> req.getServletPath().startsWith("/ws-chat")).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
