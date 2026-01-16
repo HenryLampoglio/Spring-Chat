e50cd4cca47a5b8b182bae043500cd86d8344cfd
Henry Lampoglio <henrylampoglio@hotmail.com>
2026-01-16 16:47:56 -0300

    feat: fixing tables logic

--- src/main/resources/db/migration/V1_0_1__create_user_connections_table.sql
+++ src/main/resources/db/migration/V1_0_1__create_user_connections_table.sql
@@ -1,16 +1,15 @@
 CREATE type conn_type as enum
 (
     'accepted',
-    'pending',
-    'blocked'
+    'pending'
 );
 
 CREATE TABLE user_connections (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-    user_id UUID REFERENCES users(id) NOT NULL,
-    friend_id UUID REFERENCES users(id) NOT NULL,
+    requester_id UUID REFERENCES users(id) NOT NULL,
+    receiver_id UUID REFERENCES users(id) NOT NULL,
     connection_status conn_type NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    UNIQUE (user_id, friend_id)
+    UNIQUE (requester_id, receiver_id)
 );
